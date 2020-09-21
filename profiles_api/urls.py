884c06d796419582ff3250ef81e8b60c94f1c134
Ninad K <ninadsanjaykulkarni@gmail.com>
2020-09-21 20:14:46 +0530

    Added permissions while updating the own profile.

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,13 +1,11 @@
-
-from django.contrib import admin
 from django.urls import path, include
-
-from profiles_api import views
 from rest_framework.routers import DefaultRouter
 
+from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
 
 
 urlpatterns = [
